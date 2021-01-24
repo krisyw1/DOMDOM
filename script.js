993cff568b5f52ba7744087d936fccb0b4f816ea
krisyw1 <krisyw1@gmail.com>
2021-01-24 16:35:25 +0000

    add styled div, append h1 p

--- script.js
+++ script.js
@@ -9,11 +9,23 @@ const redPara = document.createElement('p');
     redPara.textContent = "Hey I'm red!"; 
     redPara.style.color = 'red'
 
-const  blueh3= document.createElement('h3');
-    blueh3.classList.add('redPara');
+const  blueh3 = document.createElement('h3');
+    blueh3.classList.add('blueh3');
     blueh3.textContent = "Hey I'm blue h3!"; 
     blueh3.style.color = 'blue'
+
+const boxCont = document.createElement('div');
+    boxCont.classList.add('boxCont');
+    boxCont.setAttribute('style', 'background-color: pink; border:1px solid black;');
+    
+const boxH1 = document.createElement('h1');
+    boxH1.textContent = "I’m in a div";
+const boxP = document.createElement('p');
+    boxP.textContent = "ME TOO!";
 
 container.appendChild(content);
 container.appendChild(redPara);
 container.appendChild(blueh3);
+container.appendChild(boxCont);
+boxCont.appendChild(boxH1);
+boxCont.appendChild(boxP);
