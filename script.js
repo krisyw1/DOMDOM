795e99819b0ebb95f4c7b2b9b180c8c2129b2e67
krisyw1 <krisyw1@gmail.com>
2021-01-24 15:53:02 +0000

    add red  <p>

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+    content.classList.add('content');
+    content.textContent = 'This is the glorious text-content!';
+
+const redPara = document.createElement('p');
+    redPara.classList.add('redPara');
+    redPara.textContent = "Hey I'm red!"; 
+    redPara.style.color = 'red'
 
 container.appendChild(content);
+container.appendChild(redPara);
